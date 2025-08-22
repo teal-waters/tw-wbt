15339eb7063e558a28e25f632eedd23bbbe0905c
Jesse Anderson <jesse.anderson@gmail.com>
2025-08-22 13:01:59 -0700

    feat: added wrappers for all functions for twi (except twi)

--- tw_wbt/wbt.py
+++ tw_wbt/wbt.py
@@ -1,19 +1,37 @@
 """Wrappers for WhiteBoxTools functions."""
 
+import geopandas as gpd
+import xarray as xr
 from whitebox import WhiteboxTools
 
 from tw_wbt.wrapper import wrap_wbt
-
-# from xarray import DataArray
-# from xarray import Dataset
 
 wbt = WhiteboxTools()
 
 
 resample = wrap_wbt(wbt.resample)
+gaussian_filter = wrap_wbt(wbt.gaussian_filter)
+fill_burn = wrap_wbt(wbt.fill_burn)
+breach_depressions_least_cost = wrap_wbt(wbt.breach_depressions_least_cost)
+fill_depressions = wrap_wbt(wbt.fill_depressions)
+quinn_flow_accumulation = wrap_wbt(wbt.quinn_flow_accumulation)
+d_inf_flow_accumulation = wrap_wbt(wbt.d_inf_flow_accumulation)
+d8_flow_accumulation = wrap_wbt(wbt.d8_flow_accumulation)
+slope = wrap_wbt(wbt.slope)
 
 
-# def resample(
-#    input: DataArray | Dataset | list[DataArray] | list[Dataset], **kwargs: dict
-# ) -> DataArray:
-#    return _resample(input, **kwargs)
+def burn(
+    xarr: xr.DataArray | xr.Dataset, lines: gpd.GeoDataFrame
+) -> xr.DataArray | xr.Dataset:
+    """Burn lines into a Xarray object.
+
+    Args:
+        xarr: Input DataArray or Dataset
+        lines: Lines to burn
+
+    Returns:
+        The input with lines burned in.
+
+    """
+    burned_xarr = fill_burn(xarr, lines)
+    return xarr.where(burned_xarr >= xarr, burned_xarr)
